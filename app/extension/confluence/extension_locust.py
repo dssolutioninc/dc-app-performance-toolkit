057ca67597d890df7cb46c3f5d9348b605d1c9f1
Quang Nguyen <quang.nguyen@dssolution.jp>
2023-12-06 12:38:52 +0700

    test-app

--- app/extension/confluence/extension_locust.py
+++ app/extension/confluence/extension_locust.py
@@ -1,29 +1,68 @@
 import re
-from locustio.common_utils import init_logger, confluence_measure, run_as_specific_user  # noqa F401
+import json
+import random
+from locustio.common_utils import init_logger, confluence_measure, run_as_specific_user, RESOURCE_HEADERS    # noqa F401
 
 logger = init_logger(app_type='confluence')
 
 
 @confluence_measure("locust_app_specific_action")
-# @run_as_specific_user(username='admin', password='admin')  # run as specific user
+@run_as_specific_user(username='admin', password='admin')  # run as specific user
 def app_specific_action(locust):
-    r = locust.get('/app/get_endpoint', catch_response=True)  # call app-specific GET endpoint
-    content = r.content.decode('utf-8')   # decode response content
+    headers = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language': '"en-US,en;q=0.5"',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'X-Atlassian-Token': 'no-check',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Accept-Encoding': 'gzip, deflate',
+        'Authorization': 'Basic YWRtaW46YWRtaW4=',
+        'Cookie': 'INGRESSCOOKIE=8df6887f596aed234584c96ee07ad12a|bc7938bbab5da9ce7e6ee05df935b659; JSESSIONID=D28AB74C7351CDCB60518721584D0EA7'
+    }
+    # add new task
+    payload = {'task': 'new', 'key': 'HEL'}
+    files=[]
+    r = locust.post('/tfc/timesheetapplication/NewDefault.action', headers=headers, data=payload, files=files)  # call app-specific POST endpoint
+    content = json.loads(r.content.decode('utf-8'))
+    assert content['success'], 'Add new task failed'  # assertion after POST request
 
-    token_pattern_example = '"token":"(.+?)"'
-    id_pattern_example = '"id":"(.+?)"'
-    token = re.findall(token_pattern_example, content)  # get TOKEN from response using regexp
-    id = re.findall(id_pattern_example, content)    # get ID from response using regexp
+    # get task
+    payload={'start': '0',
+        'limit': '50',
+        'month': '12',
+        'year': '2023',
+        'key': 'HEL',
+        'user': 'admin'}
+    r = locust.post('/tfc/timesheetapplication/LoadDefault.action', headers=headers, data=payload, files=files)  # call app-specific POST endpoint
+    content = json.loads(r.content.decode('utf-8'))
+    assert content['success'], 'Get tasks failed'  # assertion after POST request
+    tasks = content['tasks']
 
-    logger.locust_info(f'token: {token}, id: {id}')  # log info for debug when verbose is true in confluence.yml file
-    if 'assertion string' not in content:
-        logger.error(f"'assertion string' was not found in {content}")
-    assert 'assertion string' in content  # assert specific string in response content
+    # edit task
+    taskId = random.choice(tasks)['id']
+    if taskId:
+        payload={'task': 'edit',
+            'key': 'id',
+            'keyID': f'{taskId}',
+            'field': 'title',
+            'value': f'edit task {taskId}',
+            'month': '12',
+            'year': '2023',
+            'space': 'HEL'}
+        r = locust.post('/tfc/timesheetapplication/EditDefault.action', headers=headers, data=payload, files=files)  # call app-specific POST endpoint
+        content = json.loads(r.content.decode('utf-8'))
+        assert content['success'], 'Edit task failed'  # assertion after POST request
 
-    body = {"id": id, "token": token}  # include parsed variables to POST request body
-    headers = {'content-type': 'application/json'}
-    r = locust.post('/app/post_endpoint', body, headers, catch_response=True)  # call app-specific POST endpoint
-    content = r.content.decode('utf-8')
-    if 'assertion string after successful POST request' not in content:
-        logger.error(f"'assertion string after successful POST request' was not found in {content}")
-    assert 'assertion string after successful POST request' in content  # assertion after POST request
+    # delete task
+    taskId = random.choice(tasks)['id']
+    if taskId:
+        payload={'task': 'delete',
+            'deleteKeys': f'["{taskId}"]',
+            'key': 'id',
+            'month': '12',
+            'year': '2023',
+            'space': 'HEL'}
+        r = locust.post('/tfc/timesheetapplication/DeleteDefault.action', headers=headers, data=payload, files=files)  # call app-specific POST endpoint
+        content = json.loads(r.content.decode('utf-8'))
+        assert content['success'], 'Delete task failed'  # assertion after POST request
