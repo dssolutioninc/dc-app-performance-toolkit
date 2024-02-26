0d63b1fe0efbda5d3e82dd974bbb6d0c217dd2b0
= <=>
2024-02-26 14:15:11 +0700

    Define projectkey for testing.

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -13,7 +13,7 @@ jira_dataset = jira_datasets()
 def version_manager(locust):
     ts = time.time()
     raise_if_login_failed(locust)
-    project_key = random.choice(["AANES", "AASSS", "ABICA", "ABYRAA", "AACIMA", "AAMMNA"])
+    project_key = random.choice(["AANES", "AAETAA", "AAFISA", "AAANI", "AAFAIA", "AASSS"])
     logger.locust_info(f'Testing project: {project_key}')
 
     # create version
