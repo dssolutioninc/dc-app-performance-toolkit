ab8d48098760bb40dd00aad7e39ddc34450314a4
= <=>
2024-03-01 15:28:45 +0700

    Update test api.

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -1,29 +1,22 @@
 import re
 from locustio.common_utils import init_logger, jira_measure, run_as_specific_user  # noqa F401
+from locustio.jira.requests_params import jira_datasets
+import json
+import random
+import time
 
 logger = init_logger(app_type='jira')
+jira_dataset = jira_datasets()
 
 
-@jira_measure("locust_app_specific_action")
-# @run_as_specific_user(username='admin', password='admin')  # run as specific user
-def app_specific_action(locust):
-    r = locust.get('/app/get_endpoint', catch_response=True)  # call app-specific GET endpoint
-    content = r.content.decode('utf-8')   # decode response content
+@jira_measure("locust_project_role_tab")
+@run_as_specific_user(username='admin', password='admin')  # run as specific user
+def project_role_tab(locust):
+    ts = time.time()
+    raise_if_login_failed(locust)
+    project_key = random.choice(jira_dataset['projects'])[0] #key or id
+    logger.locust_info(f'Testing project: {project_key}')
 
-    token_pattern_example = '"token":"(.+?)"'
-    id_pattern_example = '"id":"(.+?)"'
-    token = re.findall(token_pattern_example, content)  # get TOKEN from response using regexp
-    id = re.findall(id_pattern_example, content)    # get ID from response using regexp
-
-    logger.locust_info(f'token: {token}, id: {id}')  # log info for debug when verbose is true in jira.yml file
-    if 'assertion string' not in content:
-        logger.error(f"'assertion string' was not found in {content}")
-    assert 'assertion string' in content  # assert specific string in response content
-
-    body = {"id": id, "token": token}  # include parsed variables to POST request body
-    headers = {'content-type': 'application/json'}
-    r = locust.post('/app/post_endpoint', body, headers, catch_response=True)  # call app-specific POST endpoint
-    content = r.content.decode('utf-8')
-    if 'assertion string after successful POST request' not in content:
-        logger.error(f"'assertion string after successful POST request' was not found in {content}")
-    assert 'assertion string after successful POST request' in content  # assertion after POST request
+    # get group member
+    r = locust.get('/rest/projectrole/1.0/projectrole/10002/PT/groupmember?groupname=jira-administrators', catch_response=True)  # call app-specific GET endpoint //fix groupname
+    assert r.status_code == 200
