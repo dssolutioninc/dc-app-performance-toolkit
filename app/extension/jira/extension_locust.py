0f456f9024db16425efa8e2d195b714815940d23
= <=>
2024-03-04 10:27:20 +0700

    Update test api.

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -1,5 +1,5 @@
 import re
-from locustio.common_utils import init_logger, jira_measure, run_as_specific_user  # noqa F401
+from locustio.common_utils import init_logger, jira_measure, run_as_specific_user, raise_if_login_failed  # noqa F401
 from locustio.jira.requests_params import jira_datasets
 import json
 import random
@@ -18,5 +18,5 @@ def project_role_tab(locust):
     logger.locust_info(f'Testing project: {project_key}')
 
     # get group member
-    r = locust.get('/rest/projectrole/1.0/projectrole/10002/PT/groupmember?groupname=jira-administrators', catch_response=True)  # call app-specific GET endpoint //fix groupname
+    r = locust.get(f'/rest/projectrole/1.0/projectrole/10002/{project_key}/groupmember?groupname=jira-administrators', catch_response=True)  # call app-specific GET endpoint //fix project role & groupname
     assert r.status_code == 200
